585a9bde832f8126717ba6d5a027ff91d64af763
Mihail Mihailov <mihailov.m.v@gmail.com>
2016-08-28 22:03:26 +0300

    save

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,42 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*
+var awesomeThougths = "I am Mike and I am AWESOME!"
+console.log(awesomeThougths);
+var funThoughts = awesomeThougths.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+*/
+var bio = {
+  "name": "Mihail Mihailov",
+  "role": "JS developer",
+  "contacts": {
+    "email": "mihailov.m.v@gmail.com",
+    "github": "mihailov-m"
+  },
+  "welocomeMessage": "Hi!",
+  "skills": [
+    "awesomeness", "js"
+  ],
+  "bioPic": "images/fry.jpg"
+};
+
+var work = {
+  "position": "unemployed"
+}
+
+var education = {
+  "name": "ISU",
+  "years": 2010
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "blah",
+      "description": "blah-blah"
+    }
+  ]
+}
